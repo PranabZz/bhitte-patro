8cf628a7328a721eb3801cd1f4b253d71df723e4
PranabZz <pranabkca321@gmail.com>
2026-03-12 21:13:12 +0545

    Initial Commit

---
+++ NepaliPatro/NepaliPatroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NepaliPatroApp.swift
+//  NepaliPatro
+//
+//  Created by Pranab Kc on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NepaliPatroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
